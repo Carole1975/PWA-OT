23ebfac2e7a3543e81f800366615bcf08a846fa6
Carole <carole.velarde@orange.fr>
2017-09-07 10:10:16 +0200

    carte de Saint-Gaudens affichée

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -1,30 +1,33 @@
-$(document).ready(function(){
+(function(){
+console.log("map");
+  var app = {
+      init: function() {
+          this.initmap();
+      },
+      // carte interactive Carole, Julien
+      initmap: function() {
+        var map = new L.Map('cdf_map', { fullscreenControl: true });
+        var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Imagery © CloudMade';
 
-   
+        var osm = new L.TileLayer(osmUrl, { minZoom: 10, maxZoom: 19, attribution: osmAttrib });
 
-        // carte interactive Carole, Julien
-        initmap: function(data) {
-            var map = new L.Map('cdf_map', { fullscreenControl: true });
-            var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Imagery © CloudMade';
-
-            var osm = new L.TileLayer(osmUrl, { minZoom: 10, maxZoom: 19, attribution: osmAttrib });
-
-            map.setView(new L.LatLng(43.1083, 0.7234), 16);
-            map.addLayer(osm);
-            map.scrollWheelZoom.disable();
-            map.on('fullscreenchange', function() {
-                if (map.isFullscreen()) {
-                    bouton.remove();
-                    return;
-                } else {
-                    map.remove();
-                    app.init();
-                }.addTo(map);
-            });
-         };
-     });
-            
+        map.setView(new L.LatLng(43.1083, 0.7234), 16);
+        map.addLayer(osm);
+        map.scrollWheelZoom.disable();
+        map.on('fullscreenchange', function() {
+          if (map.isFullscreen()) {
+            bouton.remove();
+            return;
+          } else {
+            map.remove();
+            app.init();
+          }
+        });
+      }
+    };
+      app.init();
+  })();
 
 
            
@@ -40,7 +43,7 @@ $(document).ready(function(){
         
 
             
-    app.init();
+   
 
 
 
