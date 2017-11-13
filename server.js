1023ee4c7fb013129655ebed6d1ca93fd42de8f5
Carole <carole.velarde@orange.fr>
2017-11-13 09:53:11 +0100

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
+
+const port = 8000;
 
 
 app.use(express.static(__dirname + '/public'));
